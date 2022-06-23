8a8dd79377f0e718eee0bde46a1b8275f2430c0e
orlando167 <orlando.gutierrez@onvision.cl>
2022-06-22 21:21:55 -0400

    rutas en login, tiene error

--- ProyectoWeb/ProyectoWeb/urls.py
+++ ProyectoWeb/ProyectoWeb/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ProyectoWebApp.urls')),
     path('tienda/', include('Tienda.urls')),
+    path('login/', include('Login.urls')),
 ]
