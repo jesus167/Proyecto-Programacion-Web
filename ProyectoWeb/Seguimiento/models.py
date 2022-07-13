16c610c892babf550ae71ca58a650c62fe85e2e2
orlando167 <orlando.gutierrez@onvision.cl>
2022-07-13 17:31:43 -0400

    se agrega maqueta seguimiento sin funcionalidad

--- ProyectoWeb/Seguimiento/models.py
+++ ProyectoWeb/Seguimiento/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Tienda.models import Producto
 
 # Create your models here.
+
+
+class Order(models.Model):
+    num_order = models.CharField(max_length=10, null=True, blank=True)
+    cliente = models.CharField(max_length= 200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.num_order
+
+class OrderDetail(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cant = models.IntegerField(default=1)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.producto
